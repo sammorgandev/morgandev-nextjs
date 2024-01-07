a936f8acc3f63d427f7d9bc063157bfd5e32d9bc
Samuel Morgan <samuelmorgan@Samuels-MacBook-Pro.local>
2024-01-06 23:47:12 -0500

    More fixes

--- app/components/MainLayout.tsx
+++ app/components/MainLayout.tsx
@@ -3,15 +3,16 @@ import Footer from "./Footer";
 import BackgroundBlur from "./BackgroundBlur";
 export default function Layout({ children }: { children: React.ReactNode }) {
 	return (
-		<div className=" bg-gray-50 flex flex-col h-screen">
-			<div className="flex-none">
-				<Header />
+		<div className=" bg-gray-50 h-screen flex flex-col sticky">
+			<Header />
 
+			<div className="flex-shrink flex-grow-0">
 				<BackgroundBlur />
 			</div>
-			<div className=" bg-gray-50 grow h-fit py-8">{children}</div>
+			<div className=" bg-gray-50 flex-grow py-8">{children}</div>
 
-			<div className="flex-none">
+			<div className="flex-shrink flex-grow-0">
+				{" "}
 				<Footer />
 			</div>
 		</div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -15,8 +15,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-		<html lang="en" className="h-fit">
-			<body className={`h-fit ${inter.className}`}>{children}</body>
+		<html lang="en" className="h-full">
+			<body className={`${inter.className}`}>{children}</body>
 		</html>
 	);
 }
