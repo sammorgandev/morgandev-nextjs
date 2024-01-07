31d50990994d60c3819253edf8bde5857844ce6b
Samuel Morgan <samuelmorgan@Samuels-MacBook-Pro.local>
2024-01-06 23:06:43 -0500

    Updated footer layout

--- app/components/MainLayout.tsx
+++ app/components/MainLayout.tsx
@@ -3,7 +3,7 @@ import Footer from "./Footer";
 import BackgroundBlur from "./BackgroundBlur";
 export default function Layout({ children }: { children: React.ReactNode }) {
 	return (
-		<div className=" bg-gray-50 h-full">
+		<div className=" bg-gray-50 h-screen">
 			<Header />
 			<BackgroundBlur />
 			<div className=" bg-gray-50 h-full py-8">{children}</div>
