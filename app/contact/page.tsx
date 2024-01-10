cdc538f82a80375abb11dbca5dc273f206d1aea1
Samuel Morgan <samuelmorgan@Samuels-MacBook-Pro.local>
2024-01-09 23:24:19 -0500

    success alert location update

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -14,10 +14,6 @@ export default function Contact() {
 		<MainLayout>
 			<div className="relative isolate bg-transparent">
 				<div className="mx-auto max-w-2xl lg:max-w-4xl">
-					<div className="mb-4">
-						<SuccessAlert initialOpen={submitted} />
-					</div>
-
 					<h2 className="text-4xl text-left lg:text-center font-bold tracking-tight text-gray-900">
 						Contact
 					</h2>
@@ -89,7 +85,7 @@ export default function Contact() {
 									</label>
 									<div className="mt-2.5">
 										<input
-											type="url"
+											type="text"
 											name="website"
 											id="website"
 											className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
@@ -114,6 +110,9 @@ export default function Contact() {
 								</div>
 							</div>
 							<div className="mt-10">
+								<div className="mb-10">
+									<SuccessAlert initialOpen={submitted} />
+								</div>
 								<button
 									type="submit"
 									className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
